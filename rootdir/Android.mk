ee234e7d9a2d6b0a5d624dd11cfa10b1a271b455
Dylan Neve <dylanneve1@gmail.com>
2021-11-29 19:17:19 +0000

    universal9810: This is obsolete on S

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := android.hardware.camera.provider@2.4-service.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := root/android.hardware.camera.provider@2.4-service.rc
 LOCAL_MODULE_PATH  := etc/init
