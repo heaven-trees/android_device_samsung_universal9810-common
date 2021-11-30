0758226d3439d88fe0c571d04f29d3fe649e22da
Dylan Neve <dylanneve1@gmail.com>
2021-11-30 13:13:29 +0000

    universal9810: Add initial framework compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,6 +99,7 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/configs/init/recovery.fstab
 # Manifest
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
